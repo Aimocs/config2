0e45076955a87900473e28eb54e769e01d92f43c
Aimocs <csbot69@gmail.com>
2024-11-09 21:02:25 +0545

    batter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,3 @@
-require('telescope').setup{
-	defaults = {
-		prompt_prefix = "  ", 
-		selection_caret = "  ",
-	},
-	
-}
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
 -- or                              , branch = '0.1.x',
@@ -22,4 +9,20 @@ return {
 	      enabled = vim.g.have_nerd_font
       },
     },
+    config= function()
+
+	    require('telescope').setup{
+		    defaults = {
+			    prompt_prefix = "  ", 
+			    selection_caret = "  ",
+		    },
+
+	    }
+	    local builtin = require('telescope.builtin')
+	    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+	    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end,
 }
